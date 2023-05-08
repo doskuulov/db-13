16f3f4c3996827fe32975362b2477cde697badd2
Mirbek Doskulov <90686965+doskuulov@users.noreply.github.com>
2023-05-08 21:28:07 +0600

    Hero add

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
+<script setup>
+import Header from "./components/Layout/Header.vue";
+import Hero from "./components/Hero.vue";
+</script>
+
 <template>
   <div class="container">
     <Header />
+    <Hero />
   </div>
 </template>
-
-<script setup>
-import Header from "./components/Layout/Header.vue";
-</script>
 
 <style lang="scss">
 @import "./assets/styles/global.scss";

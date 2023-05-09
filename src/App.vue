6e706f526674a401d607103c68e0edc691a09dcb
Mirbek Doskulov <90686965+doskuulov@users.noreply.github.com>
2023-05-09 20:33:08 +0600

    Product add

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
-<script setup>
-import Header from "./components/Layout/Header.vue";
-import Hero from "./components/Hero.vue";
-import AboutBrand from "./components/AboutBrand.vue";
-</script>
-
 <template>
   <div class="container">
     <Header />
     <Hero />
     <AboutBrand />
+    <Product />
   </div>
 </template>
+
+<script setup>
+import Header from "./components/Layout/Header.vue";
+import Hero from "./components/Hero.vue";
+import AboutBrand from "./components/AboutBrand.vue";
+import Product from "./components/Products4Column.vue";
+</script>
 
 <style lang="scss">
 @import "./assets/styles/global.scss";
